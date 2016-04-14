2cc6fe9e802646c906381468b4134387ef9bb526
Niklas Fors <niklas.fors@cs.lth.se>
2016-04-14 11:03:48 +0200

    Add example TwoCounters

---
+++ examples/PrintFunction.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void Print(int in) {
+	printf("%d\n", in);
+}
+
+void PrintBool(bool b) {
+	printf("%s\n", b ? "true" : "false");
+}
+
+void PrintStrInt(const char *str, int n) {
+	printf("%s%d\n", str, n);
+}
+
+void PrintStrIntInt(const char *str, int n1, int n2) {
+	printf("%s(%d, %d)\n", str, n1, n2);
+}
+
+void PrintStrBool(const char *str, bool b) {
+	printf("%s%s\n", str, b ? "true" : "false");
+}
