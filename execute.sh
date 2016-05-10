711fac7ff1058bba39e60f880fa629a227b1b6f3
Niklas Fors <niklas.fors@cs.lth.se>
2016-05-10 12:50:36 +0200

    Make execute.sh more robust

--- execute.sh
+++ execute.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
 GEN_DIR="examples/gen/"
+JAR_FILE="bloqqi-compiler.jar"
 
 if [ $# != 1 ]; then
 	echo "Specify .dia file to execute"
 	exit 
 fi
 
+if [ ! -f $JAR_FILE ]; then
+	ant jar
+fi
+
 name=$(basename "$1")
 genfile="$GEN_DIR/${name%.*}"
 
-java -jar bloqqi-compiler.jar $1 --c --o="$genfile.c"
+java -jar $JAR_FILE $1 --c --o="$genfile.c"
 gcc -std=c99 "$genfile.c" examples/PrintFunction.c -o $genfile
 ./$genfile
