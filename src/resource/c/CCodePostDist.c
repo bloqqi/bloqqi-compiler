4f614ce74da03d35183737814cf6e985db55c751
Niklas Fors <niklas.fors@cs.lth.se>
2019-12-18 16:39:33 +0100

    dist: minor refactoring

--- src/resource/c/CCodePostDist.c
+++ src/resource/c/CCodePostDist.c
@@ -1,14 +1,3 @@
-void on_connect_failure(void* context, MQTTAsync_failureData* response);
-void bloqqi_run_main(MQTTAsync client);
-
-const char *username = NULL;
-const char *password = NULL;
-
-// Don't start the Bloqqi program until we are connected to the Broker
-volatile bool connected = false;
-pthread_mutex_t connected_lock = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t connected_cond = PTHREAD_COND_INITIALIZER;
-
 void set_connected(bool value) {
   pthread_mutex_lock(&connected_lock);
   connected = value;
@@ -68,7 +57,6 @@ MQTTAsync_connectOptions get_connect_options(MQTTAsync client) {
   return conn_opts;
 }
 
-
 void reconnect(MQTTAsync client) {
   MQTTAsync_connectOptions conn_opts = get_connect_options(client);
   int rc;
@@ -104,7 +92,6 @@ void on_connect_failure(void* context, MQTTAsync_failureData* response) {
   }
 }
 
-
 void on_connection_lost(void *context, char *cause) {
   MQTTAsync client = (MQTTAsync)context;
 
@@ -124,6 +111,99 @@ int on_message_recieved(void *context, char *topicName, int topicLen, MQTTAsync_
   MQTTAsync_free(topicName);
 
   return 1;
+}
+
+void send_message(MQTTAsync client, const char *topic, char *payload, bool retain) {
+  MQTTAsync_responseOptions opts = MQTTAsync_responseOptions_initializer;
+  MQTTAsync_message pubmsg = MQTTAsync_message_initializer;
+  int rc;
+
+  opts.context = client;
+  pubmsg.payload = payload;
+  pubmsg.payloadlen = (int)strlen(payload) + 1;
+  pubmsg.qos = 0;
+  pubmsg.retained = retain;
+
+  if ((rc = MQTTAsync_sendMessage(client, topic, &pubmsg, &opts)) != MQTTASYNC_SUCCESS) {
+    printf("Failed to start send_message, return code %d\n", rc);
+  }
+}
+
+void send_message_int(MQTTAsync client, const char *topic, int value, bool retain) {
+  char payload[20];
+  sprintf(payload, "%d", value);
+  send_message(client, topic, payload, retain);
+}
+
+void send_message_double(MQTTAsync client, const char *topic, double value, bool retain) {
+  char payload[20];
+  sprintf(payload, "%lf", value);
+  send_message(client, topic, payload, retain);
+}
+
+/**
+ * Input functions
+ *
+ * Function adapted from:
+ * https://wiki.sei.cmu.edu/confluence/display/c/ERR34-C.+Detect+errors+when+converting+a+string+to+a+number
+ */
+bool read_int(const char *s, int *ip) {
+  char *end;
+  errno = 0;
+
+  const long sl = strtol(s, &end, 10);
+  bool ok = false;
+
+  if (end == s) {
+    fprintf(stderr, "Input error: '%s' not a decimal number\n", s);
+  } else if (*end != '\0') {
+    fprintf(stderr, "Input warning: '%s' extra characters at end of input '%s'\n", s, end);
+    ok = true;
+  } else if ((sl == LONG_MIN || sl == LONG_MAX) && errno == ERANGE) {
+    fprintf(stderr, "Input error: '%s' out of range of type long\n", s);
+  } else if (sl > INT_MAX) {
+    fprintf(stderr, "Input error: '%ld' greater than INT_MAX\n", sl);
+  } else if (sl < INT_MIN) {
+    fprintf(stderr, "Input error: '%ld' less than INT_MIN\n", sl);
+  } else {
+    ok = true;
+  }
+  if (ok) {
+    *ip = (int) sl;
+  }
+  return ok;
+}
+
+bool read_double(const char *s, double *dp) {
+  char *end;
+  errno = 0;
+
+  const double d = strtod(s, &end);
+  bool ok = false;
+
+  if (end == s) {
+    fprintf(stderr, "Input error: '%s' not a floating point number\n", s);
+  } else if (*end != '\0') {
+    fprintf(stderr, "Input warning '%s' extra characters at end of input '%s'\n", s, end);
+    ok = true;
+  } else if ((d == -HUGE_VAL || d == HUGE_VAL) && errno == ERANGE) {
+    fprintf(stderr, "Input error: '%s' out of range of type double\n", s);
+  } else {
+    ok = true;
+  }
+  if (ok) {
+    *dp = d;
+  }
+  return ok;
+}
+
+/**
+ * Time funcitons
+ */
+static uint64_t useconds() {
+  struct timeval v;
+  gettimeofday(&v, NULL);
+  return v.tv_sec*1000000ull + v.tv_usec;
 }
 
 uint64_t my_sleep(
@@ -146,7 +226,9 @@ uint64_t my_sleep(
   return extra;
 }
 
-
+/**
+ * Main function
+ */
 int main(int argc, char* argv[]) {
   char *address;
   if (argc == 1) {
--- src/resource/c/CCodePreDist.c
+++ src/resource/c/CCodePreDist.c
@@ -11,88 +11,21 @@
 #include <pthread.h>
 #include "MQTTAsync.h"
 
-static uint64_t useconds() {
-  struct timeval v;
-  gettimeofday(&v, NULL);
-  return v.tv_sec*1000000ull + v.tv_usec;
-}
+bool read_int(const char *s, int *ip);
+bool read_double(const char *s, double *dp);
+static uint64_t useconds();
 
-void send_message(MQTTAsync client, const char *topic, char *payload, bool retain) {
-  MQTTAsync_responseOptions opts = MQTTAsync_responseOptions_initializer;
-  MQTTAsync_message pubmsg = MQTTAsync_message_initializer;
-  int rc;
+void send_message(MQTTAsync client, const char *topic, char *payload, bool retain);
+void send_message_int(MQTTAsync client, const char *topic, int value, bool retain);
+void send_message_double(MQTTAsync client, const char *topic, double value, bool retain);
+void on_connect_failure(void* context, MQTTAsync_failureData* response);
+void bloqqi_run_main(MQTTAsync client);
 
-  opts.context = client;
-  pubmsg.payload = payload;
-  pubmsg.payloadlen = (int)strlen(payload) + 1;
-  pubmsg.qos = 0;
-  pubmsg.retained = retain;
+// Broker username and password
+const char *username = NULL;
+const char *password = NULL;
 
-  if ((rc = MQTTAsync_sendMessage(client, topic, &pubmsg, &opts)) != MQTTASYNC_SUCCESS) {
-    printf("Failed to start send_message, return code %d\n", rc);
-  }
-}
-
-void send_message_int(MQTTAsync client, const char *topic, int value, bool retain) {
-  char payload[20];
-  sprintf(payload, "%d", value);
-  send_message(client, topic, payload, retain);
-}
-
-void send_message_double(MQTTAsync client, const char *topic, double value, bool retain) {
-  char payload[20];
-  sprintf(payload, "%lf", value);
-  send_message(client, topic, payload, retain);
-}
-
-// Function adapted:
-// from https://wiki.sei.cmu.edu/confluence/display/c/ERR34-C.+Detect+errors+when+converting+a+string+to+a+number
-bool read_int(const char *s, int *ip) {
-  char *end;
-  errno = 0;
-
-  const long sl = strtol(s, &end, 10);
-  bool ok = false;
-
-  if (end == s) {
-    fprintf(stderr, "Input error: '%s' not a decimal number\n", s);
-  } else if (*end != '\0') {
-    fprintf(stderr, "Input warning: '%s' extra characters at end of input '%s'\n", s, end);
-    ok = true;
-  } else if ((sl == LONG_MIN || sl == LONG_MAX) && errno == ERANGE) {
-    fprintf(stderr, "Input error: '%s' out of range of type long\n", s);
-  } else if (sl > INT_MAX) {
-    fprintf(stderr, "Input error: '%ld' greater than INT_MAX\n", sl);
-  } else if (sl < INT_MIN) {
-    fprintf(stderr, "Input error: '%ld' less than INT_MIN\n", sl);
-  } else {
-    ok = true;
-  }
-  if (ok) {
-    *ip = (int) sl;
-  }
-  return ok;
-}
-
-bool read_double(const char *s, double *dp) {
-  char *end;
-  errno = 0;
-
-  const double d = strtod(s, &end);
-  bool ok = false;
-
-  if (end == s) {
-    fprintf(stderr, "Input error: '%s' not a floating point number\n", s);
-  } else if (*end != '\0') {
-    fprintf(stderr, "Input warning '%s' extra characters at end of input '%s'\n", s, end);
-    ok = true;
-  } else if ((d == -HUGE_VAL || d == HUGE_VAL) && errno == ERANGE) {
-    fprintf(stderr, "Input error: '%s' out of range of type double\n", s);
-  } else {
-    ok = true;
-  }
-  if (ok) {
-    *dp = d;
-  }
-  return ok;
-}
+// Connected flag
+volatile bool connected = false;
+pthread_mutex_t connected_lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t connected_cond = PTHREAD_COND_INITIALIZER;
